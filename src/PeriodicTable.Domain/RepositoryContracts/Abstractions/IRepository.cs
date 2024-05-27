b7b059c0962ab96f6a0de458363c49a7b24d9ddf
savchenkoadd <savchenkooad@gmail.com>
2024-05-27 07:28:00 +0300

    Add Repository Contracts

---
+++ src/PeriodicTable.Domain/RepositoryContracts/Abstractions/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace PeriodicTable.Domain.RepositoryContracts.Abstractions
+{
+	public interface IRepository<TEntity, TKey>
+	{
+		Task<TEntity> GetById(TKey key);
+
+		Task<IEnumerable<TEntity>> GetAll();
+
+		Task Add(TEntity item);
+
+		Task Update(TKey key, TEntity item);
+
+		Task Delete(TKey key);
+	}
+}
---
+++ src/PeriodicTable.Domain/RepositoryContracts/IElementsRepository.cs
@@ -0,0 +1,10 @@
+﻿using PeriodicTable.Domain.Entities;
+using PeriodicTable.Domain.RepositoryContracts.Abstractions;
+
+namespace PeriodicTable.Domain.RepositoryContracts
+{
+	public interface IElementsRepository : IRepository<ChemicalElement, Guid>
+	{
+		Task<IEnumerable<ChemicalElement>> SearchBy(Predicate<ChemicalElement> predicate);
+	}
+}
