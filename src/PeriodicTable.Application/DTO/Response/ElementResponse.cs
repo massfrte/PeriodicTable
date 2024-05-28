be0398d736ac6ee7ec07bf6c38c4d723cf648256
savchenkoadd <savchenkooad@gmail.com>
2024-05-28 18:41:38 +0300

    Fix issues with Update functionality

--- src/PeriodicTable.Application/DTO/Response/ElementResponse.cs
+++ src/PeriodicTable.Application/DTO/Response/ElementResponse.cs
@@ -1,25 +1,43 @@
-﻿namespace PeriodicTable.Application.DTO.Response
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeriodicTable.Application.DTO.Response
 {
 	public class ElementResponse
 	{
 		public Guid Id { get; set; }
 
+		[Required]
+		[StringLength(10)]
 		public string Symbol { get; set; }
 
+		[Required]
+		[StringLength(30)]
 		public string Name { get; set; }
 
+		[Required]
+		[StringLength(50)]
 		public string Group { get; set; }
 
+		[Required]
+		[StringLength(30)]
 		public string StandartState { get; set; }
 
+		[Required]
+		[Range(1, int.MaxValue)]
 		public int AtomicNumber { get; set; }
 
+		[Required]
+		[Range(0, float.MaxValue)]
 		public float Electronegativity { get; set; }
 
+		[Required]
 		public float MeltingPointInK { get; set; }
 
+		[Required]
 		public float BoilingPointInK { get; set; }
 
+		[Required]
+		[Range(0, 2024)]
 		public short YearDiscovered { get; set; }
 	}
 }
